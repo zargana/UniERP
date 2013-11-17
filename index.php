5248f2951e551396d79ddb28a3b0bd855272c374
Hasan Tokatlı <hasantktl@gmail.com>
2013-11-17 09:56:08 +0200

    indexte ornek nesne olusturma yapildi.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 include_once 'config.php';
 
+include_once 'lib/Ogrenci.php';
+
 $Ogrenci = new Ogrenci();
 
+$Ogrenci->isim = 'Hasan';
+$Ogrenci->soyisim = 'Tokatlı';
+
+echo $Ogrenci->getirUzunIsım();
